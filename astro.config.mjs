3e4c40c459abea1cedb66ef8e64644c87f96d9dc
yufanglin0919 <yufanglin0919@gmail.com>
2026-05-25 17:25:33 +0800

    fix astro site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'womaowujing-investment-reports-6mr4-uyo92k6hf.vercel.app',
+	site: 'https://womaowujing-investment-reports-6mr4.vercel.app',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
