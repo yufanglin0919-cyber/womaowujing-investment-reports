700b818a3cea10083e95e99ee6ebbc15e31c1a2b
yufanglin0919 <yufanglin0919@gmail.com>
2026-05-31 01:50:56 +0800

    Update public portfolio content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Yufang Lin | Management Engineering & Data Analysis';
+export const SITE_DESCRIPTION =
+  'Personal portfolio for management engineering, data analysis, Python workflows, and decision modeling.';
